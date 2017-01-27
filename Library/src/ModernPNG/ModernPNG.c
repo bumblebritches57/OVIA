6d27ad87881c07e43f3d0fbcd8c483d51829edbe
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2017-01-27 10:46:12 -0500

    Reorganized file hierarchy
    Redid the Xcode project because it couldn’t cope with the rename, and
    fixed the SeekBits -> SkipBits issue

---
+++ Library/src/ModernPNG/ModernPNG.c
@@ -0,0 +1,24 @@
+#include "libModernPNG.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    int main(int argc, const char * argv[]) {
+        BitInput  *BitI    = calloc(sizeof(BitInput), 1);
+        BitOutput *BitO    = calloc(sizeof(BitOutput), 1);
+        ErrorStatus *Error = calloc(sizeof(ErrorStatus), 1);
+        PNGDecoder *PNG    = calloc(sizeof(PNGDecoder), 1);
+        
+        InitBitInput(BitI, Error, argc, argv);
+        InitBitOutput(BitO, Error, argc, argv);
+        InitPNGDecoder(PNG);
+        ParsePNG(BitI, PNG);
+        
+        // DecodePNG(BitI, PNG);
+        return 0;
+    }
+    
+#ifdef __cplusplus
+}
+#endif
