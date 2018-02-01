06c7de9ea7757c2c49324b72c69ae81f98a50dae
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2018-02-01 11:25:26 -0500

    Updated BitIO and the API calls, but this isn't really strategically important anymore so I'm probably not gonna finish it.

--- Library/src/Common/libModernFLAC_Common.c
+++ Library/src/Common/libModernFLAC_Common.c
@@ -1,9 +1,17 @@
 #include "../../include/Private/Common/libModernFLAC_Common.h"
 
-inline int64_t OnesCompliment2TwosCompliment(const int64_t OnesCompliment) {
-    return ~OnesCompliment + 1;
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+    inline int64_t OnesCompliment2TwosCompliment(const int64_t OnesCompliment) {
+        return ~OnesCompliment + 1;
+    }
+    
+    inline int64_t TwosCompliment2OnesCompliment(const int64_t TwosCompliment) {
+        return TwosCompliment ^ 0xFFFFFFFFFFFFFFFF;
+    }
+    
+#ifdef __cplusplus
 }
-
-inline int64_t TwosCompliment2OnesCompliment(const int64_t TwosCompliment) {
-    return TwosCompliment ^ 0xFFFFFFFFFFFFFFFF;
-}
+#endif
