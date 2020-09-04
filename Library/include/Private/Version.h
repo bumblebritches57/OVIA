30e0d597797bcb074efe12af849dc79a6355da78
Marcus Johnson <MarcusLJohnson1991@gmail.com>
2020-09-04 06:11:08 -0700

    Somehow the PNG code got dropped, added it back.

---
+++ Library/include/Private/Version.h
@@ -0,0 +1,18 @@
+/*!
+ @header          Version.h
+ @author          Marcus Johnson
+ @copyright       2020+
+ @version         1.0.0
+ @brief           This header contains macros that define OVIA's version.
+ */
+
+#pragma once
+
+#ifndef OVIA_Version_H
+#define OVIA_Version_H
+
+#define OVIA_Version_Major 0
+#define OVIA_Version_Minor 7
+#define OVIA_Version_Patch 9
+
+#endif /* OVIA_Version_H */
